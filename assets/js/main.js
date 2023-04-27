490800515061a5e43b0762b1531d6f4a68ef8172
kunalsathe18 <kunalsathe18@gmail.com>
2023-04-27 12:20:04 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+let num1= 24;
+let num2= 24;
+console.log(num1==num2);
+
+let num3= 24;
+let num4= 25;
+console.log(num3==num4);
+
+let num5= 24;
+let num6= 24;
+console.log(num5=="24");
+
+let num7= 24;
+let num8=24;
+console.log(num7===num8);
+
+let num9= 24;
+let num10=24;
+console.log(num7==="num8");
+
+let num11= 24;
+let num12= 24;
+console.log(num11>num12); 
+
+let num13= 24;
+let num14= 25;
+console.log(num13<num14); 
+
+let num15= 24;
+let num16= 26;
+console.log(num15>=num16);
+
+let num17= 30;
+let num18= 28;
+console.log(num17>=num18);
